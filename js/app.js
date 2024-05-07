46d96b06fafdcd88f8a58ef3b8546bdd5c07a5d8
Jennifer Nordell <subdev338@gmail.com>
2024-05-06 23:55:25 -0400

    initial variables declared and initialized

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const keyboard = document.getElementById('qwerty');
+const phraseDisplay = document.getElementById('phrase');
+const resetButton = document.getElementById('btn__reset');
+let numberMissed = 0;
+
+const phrases = [
+    "mad as a hatter",
+    "bad moon on the rise",
+    "every cloud has a silver lining",
+    "tomorrow never knows",
+    "time waits for no one"
+];
+
